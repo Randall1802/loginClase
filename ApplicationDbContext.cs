413bec72f2bced967851aae5b48681f397929ebe
rhs-dnu <rhs@dnu.com.mx>
2024-09-03 09:56:21 -0600

    Agregar archivos de proyecto.

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace loginClase
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+			//AQUI VAN LLAVES PRIMARIAS Y FORANEAS
+
+		}
+
+		//Van los modelos que se convertiran en tablas. Modelo va en singular, pero a bd sera en plural.
+		//ej: Cliente, se manda como Clientes.
+
+	}
+}
